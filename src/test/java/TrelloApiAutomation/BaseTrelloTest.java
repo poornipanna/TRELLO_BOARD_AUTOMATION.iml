5c82000acb007bf19256d3f058996ff18e8751cd
vapasi <vapasi@vapasis-MacBook-Pro-2.local>
2019-05-08 21:30:10 +0530

    Added new files with implementation of Trello API

---
+++ src/test/java/TrelloApiAutomation/BaseTrelloTest.java
@@ -0,0 +1,116 @@
+package TrelloApiAutomation;
+
+import groovy.json.JsonException;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.json.simple.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.requestSpecification;
+
+public class BaseTrelloTest {
+
+    String boardId;
+    String cardId;
+    String listName;
+    final String key = "0697ace29da135af1009cc535346c753 ";
+    final String token = "4796c4db8c0b4d5f548a6ac73389331985e5e9eb7c6f6e588deb3b36062fa13f ";
+    JSONObject requestJSon = new JSONObject();
+    ArrayList<String> listIds= new ArrayList<String>();
+    String listId;
+
+    @BeforeSuite
+    public void setup() {
+
+        RestAssured.baseURI = "https://api.trello.com";
+
+
+        CreateBoard();
+    }
+
+    @AfterSuite
+    public void TearDown()
+    {
+        DeleteBoard();
+    }
+
+
+    public void CreateBoard() {
+
+
+        String name= "Things to Do";
+
+       // fillQueryParams();
+
+        /*
+        requestSpecification = given()
+                .body(requestJSon.toJSONString())
+                .log().all()
+                .contentType(ContentType.JSON);
+
+         */
+
+        RequestSpecification requestSpecification = given()
+                .queryParam("key", "0697ace29da135af1009cc535346c753")
+                .queryParam("token", "4796c4db8c0b4d5f548a6ac73389331985e5e9eb7c6f6e588deb3b36062fa13f")
+                .queryParam("name", name)
+                .body(requestJSon.toJSONString())
+                .log().all()
+                .contentType(ContentType.JSON);
+
+        Response response = requestSpecification.when().post("/1/boards");
+        System.out.println(response.body());
+
+        response.then().statusCode(200).contentType(ContentType.JSON).extract()
+                    .response().body().jsonPath().getMap("$");
+
+        Map<String, ?> map = response.body().jsonPath().getMap("$");
+        boardId = (String) map.get("id");
+
+        System.out.println("From map" + map.get("id"));
+        System.out.println("From map" + map.get("name"));
+        Assert.assertEquals(map.get("name"), name);
+
+    }
+
+    //Deleting board
+    public void DeleteBoard() {
+        requestSpecification = given().queryParam("key", "0697ace29da135af1009cc535346c753")
+                .queryParam("token", "4796c4db8c0b4d5f548a6ac73389331985e5e9eb7c6f6e588deb3b36062fa13f")
+                .pathParam("id", boardId).log().all()
+                .contentType(ContentType.JSON);
+
+        Response response = requestSpecification.when().delete("/1/boards/{id}");
+     //   Response response = requestSpecification.when().delete("https://api.trello.com/1/boards/{id}");
+        System.out.println(response.body());
+
+        response.then().statusCode(200).contentType(ContentType.JSON).extract()
+                .response().body().jsonPath().getMap("$");
+
+
+    }
+
+
+    public JSONObject fillQueryParams()
+    {
+        JSONObject requestJSon = new JSONObject();
+
+        try {
+
+            requestJSon.put("key", "0697ace29da135af1009cc535346c753");
+            requestJSon.put("token", "4796c4db8c0b4d5f548a6ac73389331985e5e9eb7c6f6e588deb3b36062fa13f");
+            requestJSon.put("name", "Things to do");
+        } catch (JsonException e) {
+            e.printStackTrace();
+        }
+
+        return requestJSon;
+    }
+}
---
+++ src/test/java/TrelloApiAutomation/CreateTrelloBoardTest.java
@@ -0,0 +1,148 @@
+package TrelloApiAutomation;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+
+import java.util.*;
+
+import static io.restassured.RestAssured.*;
+
+public class CreateTrelloBoardTest extends BaseTrelloTest
+{
+
+
+    @Test(priority=1)
+    public void GetListsOfBoard()
+    {
+
+        Response response =  given()
+                .pathParam("boardID",boardId)
+                .queryParam("key", key)
+                .queryParam("token", token).
+                        when().
+                        get("boards/{boardID}/lists/");
+
+        response.then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .extract();
+
+        System.out.println(response.statusCode());
+
+        List<Map<String, ?>> listInfo = response.jsonPath().getList("$");
+        for(int i = 0; i < listInfo.size(); i++) {
+            System.out.println(listInfo.get(i).get("id").toString());
+            System.out.println(listInfo.get(i).get("name").toString());
+        }
+        listId = listInfo.get(0).get("id").toString();
+        listName = listInfo.get(0).get("name").toString();
+        System.out.println("List ID: " + listId);
+        System.out.println("List Name: " + listName);
+
+
+    }
+
+    @Test(priority=2)
+    public void createCardInList()
+    {
+
+        RequestSpecification requestSpecification = given()
+                .queryParam("key", key)
+                .queryParam("token", token)
+                .queryParam("name", "CardName")
+                .queryParam("idList", listId)
+                .contentType(ContentType.JSON)
+                .log().all();
+
+        Response response = requestSpecification.when().
+                post("cards");
+        System.out.println("Response Body: " + response.body());
+        System.out.println("Response Status Code: " + response.statusCode());
+
+
+        response.then()
+                .statusCode(200);
+            /*    .contentType(ContentType.JSON)
+                .extract()
+                 .response()
+                .jsonPath()
+                .getMap("$");*/
+        Map<Object, Object> map = response.jsonPath().getMap("$");
+
+        cardId = map.get("id").toString();
+        System.out.println(cardId);
+
+    }
+
+
+
+    //@Test
+    public void updateCardDetails()
+    {
+
+        System.out.println("updateCardDetails");
+        String newNameOfCard= "Send a Mail";
+
+        /*
+        requestSpecification = given()
+                .body(requestJSon.toJSONString())
+                .log().all()
+                .contentType(ContentType.JSON);
+
+        */
+
+        RequestSpecification requestSpecification = given()
+                .queryParam("key", key)
+                .queryParam("token", token)
+                .pathParam("id", cardId)
+                .queryParam("name",newNameOfCard)
+                .body(requestJSon.toJSONString())
+                .log().all()
+                .contentType(ContentType.JSON);
+
+        Response response = requestSpecification.when().
+                put("1/cards/{cardId}");
+        response.then()
+                .statusCode(200);
+
+        Map<Object, Object> map = response.jsonPath().getMap("$");
+
+        cardId = map.get("id").toString();
+        System.out.println("CardID: " + cardId);
+
+
+    }
+
+    //@Test
+    public void deleteCard()
+    {
+        System.out.println("deleteCard");
+        RequestSpecification requestSpecification = given()
+                .queryParam("key", key)
+                .queryParam("token", token)
+                .pathParam("id", cardId)
+                .body(requestJSon.toJSONString())
+                .log().all()
+                .contentType(ContentType.JSON);
+
+        Response response = requestSpecification.when().delete("/1/cards/{cardId}");
+
+        System.out.println(response.body());
+
+        response.then().statusCode(200).contentType(ContentType.JSON).extract()
+                .response().body().jsonPath().getMap("$");
+
+
+    }
+
+
+}
